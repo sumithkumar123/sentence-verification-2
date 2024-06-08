cd42df3d5c4891bc3a562f686231611173884813
sumith <sumith95023@gmail.com>
2024-06-08 13:56:55 +0530

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
 
         <Routes>
-          <Route path="/" element={<StartingScreen />} />
+          {/* <Route path="/" element={<StartingScreen />} /> */}
           <Route path="/game" element={<Game />} />
         </Routes>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AudioIcon from './AudioIcon'; // Adjust the path as needed
 
 export default function Navbar({ attempts, elapsedTime, currentScreen, totalScreens }) {
+  const [showInstructions, setShowInstructions] = useState(false);
+  const instructionsText = " Choose the image that best mathches the passage."; // Replace with your actual instructions
+
+  const toggleInstructions = () => {
+    setShowInstructions(!showInstructions);
+  };
+
   return (
     <div className="navbar-container">
       <div className="navbar-title">
-        <h1>Sentence Verification</h1>
+        <h1>Sentence Verification Global</h1>
       </div>
-      {/* <div className="navbar-info">
-        <div className="info-item">
-          <h3>Attempts: <span>{attempts}</span></h3>
-        </div>
-        <div className="info-item">
-          <h3>Time: <span style={{ color: 'red' }}>{Math.floor(elapsedTime / 1000)}<span style={{ color: 'black' }}> seconds</span></span></h3>
-        </div>
-      </div> */}
-      <div className="navbar-screens">
+      <div className="navbar-screens" >
         <h5>Screens: <span>{currentScreen}/{totalScreens}</span></h5>
+        <div className="instructions-container">
+          <button onClick={toggleInstructions} className="instructions-button">Instructions</button>
+          {showInstructions && (
+            <div className="instructions-dropdown">
+              <p>{instructionsText}</p>
+              {/* Add more instructions as needed */}
+            </div>
+          )}
+          <AudioIcon sentence={instructionsText} />
+        </div>
       </div>
     </div>
   );

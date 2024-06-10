130bcb41e76dc4580d0b4752830c509ef14608d5
sumith <sumith95023@gmail.com>
2024-06-10 10:56:34 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import StartingScreen from './components/StartingScreen';
+// import { Routes, Route } from 'react-router-dom';
 import Game from './components/Game';
 import './App.css';
 
 function App() {
   return (
 
-        <Routes>
-          {/* <Route path="/" element={<StartingScreen />} /> */}
-          <Route path="/game" element={<Game />} />
-        </Routes>
+        // <Routes>
+        //   {/* <Route path="/" element={<StartingScreen />} /> */}
+        //   <Route path="/game" element={<Game />} />
+        // </Routes>'
+        <Game/>
   );
 }
 
